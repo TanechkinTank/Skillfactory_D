973ad3027d77c1e035897e80cffb85a37cbc8d84
Tatiana Sosenkova <108412081+TanechkinTank@users.noreply.github.com>
2023-01-19 22:37:46 +0300

    Add files via upload

--- NewsPortal/NewsPortal/urls.py
+++ NewsPortal/NewsPortal/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('news/', include('News.urls')),
     path('news/search/', include('News.urls')),
